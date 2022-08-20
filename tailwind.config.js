78cda9c078af5baabedeb4978b323c3515fc3b70
cellle <91531988+celinerh@users.noreply.github.com>
2022-08-20 14:47:49 +0200

    customize letter spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      letterSpacing: {
+        widest: ".22em",
+      },
+    },
   },
   plugins: [],
 };
