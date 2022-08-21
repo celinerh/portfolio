4b3cfa02fab06882e6eceecf68a94e8ff6e7639c
cellle <91531988+celinerh@users.noreply.github.com>
2022-08-21 16:51:33 +0200

    add about page

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import About from "./pages/About";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}></Route>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<About />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ src/pages/About.jsx
@@ -0,0 +1,22 @@
+import Heading from "../components/Heading";
+
+const About = () => {
+  return (
+    <>
+      <div className="p-10">
+        <Heading title="Celine Holvert" />
+        <p className="text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod
+          tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrum exercitationem ullam corporis suscipit
+          laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
