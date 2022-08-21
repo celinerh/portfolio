d6972ad1c49f46c1e376793f7914f26e5bd16bd7
cellle <91531988+celinerh@users.noreply.github.com>
2022-08-21 16:50:18 +0200

    add outlet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navigation from "./components/Navigation/Navigation";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Outlet />
     </div>
   );
 }
