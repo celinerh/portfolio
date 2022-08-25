dda71c34dc4706f518295677d54d4f95f8710c45
cellle <91531988+celinerh@users.noreply.github.com>
2022-08-25 13:24:29 +0200

    fix burgermenu color and margin

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -80,12 +80,18 @@ export default function Example() {
                 </div>
                 <div className="flex items-center ml-auto -mr-2 sm:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 mr-3 text-gray-400 rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
-                      <XIcon className="block w-6 h-6" aria-hidden="true" />
+                      <XIcon
+                        className="block w-6 h-6 text-zinc-600"
+                        aria-hidden="true"
+                      />
                     ) : (
-                      <MenuIcon className="block w-6 h-6" aria-hidden="true" />
+                      <MenuIcon
+                        className="block w-6 h-6 text-zinc-600"
+                        aria-hidden="true"
+                      />
                     )}
                   </Disclosure.Button>
                 </div>
