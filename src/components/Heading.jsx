3cdb6deeb3d0cc37a5ab503382f635fb481d3eb0
cellle <91531988+celinerh@users.noreply.github.com>
2022-08-25 15:27:09 +0200

    change heading background color to purple

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,7 +2,7 @@ const Heading = ({ title, className }) => {
   return (
     <h1 className={`relative inline-block mb-10 font-AbhayaLibre ${className}`}>
       {title}
-      <span className="absolute w-3/5 h-2/5 bottom-1 -left-1 -z-10 bg-amber-100"></span>
+      <span className="absolute w-3/5 bg-purple-100 h-2/5 bottom-1 -left-1 -z-10"></span>
     </h1>
   );
 };
