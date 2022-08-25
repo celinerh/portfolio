8dfd487da60a7bac02b36c2e0d99168247c32b6a
cellle <91531988+celinerh@users.noreply.github.com>
2022-08-25 13:16:37 +0200

    fix spacing and keys

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,30 +5,30 @@ const Projects = () => {
   const projects = [
     {
       href: "https://www.google.com",
-      title: "Test",
+      title: "Test1",
       imageSource: "https://picsum.photos/id/1/200/300",
-      imageAlt: "Test",
+      imageAlt: "Test1",
       text: "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.",
     },
     {
       href: "https://www.google.com",
-      title: "Test",
+      title: "Test2",
       imageSource: "https://picsum.photos/id/1/200/300",
-      imageAlt: "Test",
+      imageAlt: "Test2",
       text: "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.",
     },
     {
       href: "https://www.google.com",
-      title: "Test",
+      title: "Test3",
       imageSource: "https://picsum.photos/id/1/200/300",
-      imageAlt: "Test",
+      imageAlt: "Test3",
       text: "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.",
     },
   ];
 
   return (
-    <div className="mx-24">
-      <Heading title="Projekter" />
+    <div className="mx-6 xs:mx-12 sm:mx-16 md:mx-20 lg:mx-24 ">
+      <Heading title="Projekter" className="text-3xl md:text-5xl" />
       <div className="grid grid-cols-3 gap-10">
         {projects.map((project) => (
           <ProjectCard
